4257fc967692c2f33974273358c532a082ddade5
GarciaPena-Loris <loris.garcia-pena@etu.umontpellier.fr>
2020-12-03 12:48:31 +0100

    ajout des photos ekip

--- phpGoal/view/view.php
+++ phpGoal/view/view.php
@@ -7,7 +7,14 @@
 </head>
 <body>
     <header>
-        
+        <nav>
+            <a href="index.php?action=readAll&controller=voiture">Liste Voiture</a>
+            <a href="index.php?action=readAll&controller=utilisateur">Liste Utilisateur</a>
+            <a href="index.php?action=readAll&controller=trajet">Liste Trajet</a>
+            <a href="index.php?action=create&controller=voiture">Créer Voiture</a>
+            <a href="index.php?action=create&controller=utilisateur">Créer Utilisateur</a>
+            <a href="index.php?action=create&controller=trajet">Créer Trajet</a>
+        </nav>
     </header>
     <main>
         <?php
@@ -16,7 +23,9 @@
         ?>
     </main>
     <footer>
-        
+        <p>
+            Site de covoiturage de NABERT ENZO
+        </p>
     </footer>
 
 </body>
